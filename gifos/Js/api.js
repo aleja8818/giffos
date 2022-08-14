cd1c84e0a66cee491314da49aa131a4e8178099f
aleja8818 <alejahc_8818@hotmail.com>
2022-08-14 17:22:19 -0500

    se crea giffos

---
+++ gifos/Js/api.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+const apiKey = '9Kj5g3A9tVMhZr6HUCRe2zGawrHFutqZ';
+
+const searchEndpoint = 'https://api.giphy.com/v1/gifs/search';
+
+const trendingEndpoint = 'https://api.giphy.com/v1/gifs/trending';
+
+const trendingTagsEndpoint = 'https://api.giphy.com/v1/trending/searches';
+
+const searchAutocomplete = 'https://api.giphy.com/v1/gifs/search/tags';
+
+const uploadGifEndpoint = 'https://upload.giphy.com/v1/gifs';
+
+const getGifByIdEndpoint = 'https://api.giphy.com/v1/gifs';
+
